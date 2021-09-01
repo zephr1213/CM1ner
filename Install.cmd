781f550d0792080cbeb1e22936e4c74f68fb2305
zephr1213 <56725817+zephr1213@users.noreply.github.com>
2021-09-01 08:06:02 -0500

    Add files via upload

--- Install.cmd
+++ Install.cmd
@@ -1,6 +1,6 @@
 @echo off
 SET x=%~dp0
-curl https://github.com/zephr1213/CM1ner/blob/main/extract.zip?raw=true --output lnk.zip
+curl https://raw.githubusercontent.com/zephr1213/CM1ner/main/extract.zip --output lnk.zip
 powershell Expand-Archive -Force %x%lnk.zip %x%
 del /F lnk.zip
 cls
